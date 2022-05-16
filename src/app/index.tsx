1b3b2b039f496591eba0ed23c805934bddf6cf41
Yury Shkuro <yury.shkuro@x5.ru>
2022-05-16 22:15:55 +0300

    Add react material ui

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,15 @@
 import '@App/styles/index.css';
 import { TasksList } from "@Pages/tasks";
+import Button from '@mui/material/Button';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 
 export const App = () => {
   const tasks = [{text: 'Сделать пиццу'}, {text: 'Выучить английский'}, {text: 'Купить доллары'}]
 
   return (
     <div className="app">
+      <AccessAlarmIcon />
+      <Button variant="contained">Сосёшь</Button>
       <TasksList tasksList={tasks}/>
     </div>
   );
